2f31baf6823f8d58059fc925aa1fdf4b631b3e61
Gonza Fernandez <gonzalo.fernandez1@davinci.edu.ar>
2020-07-07 22:40:12 -0300

    mas scripts

---
+++ New Unity Project/Assets/Scripts/Nicknamo.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using Photon.Realtime;
+using TMPro;
+public class Nicknamo : MonoBehaviourPun
+{
+    public TextMeshProUGUI txt;
+    private void Awake()
+    {
+
+        if (photonView.IsMine)
+        {
+            photonView.RPC("SetMyNickName", RpcTarget.AllBuffered);
+        }
+    }
+
+    [PunRPC]
+
+    private void SetMyNickName()
+    {
+        txt.text = PhotonNetwork.LocalPlayer.NickName;
+    }
+}
+
---
+++ New Unity Project/Assets/Scripts/Spawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.UI;
+using TMPro;
+using System;
+
+public class Spawner : MonoBehaviourPunCallbacks
+{
+    private MyServer _myserver;
+    public Animator winScreen;
+
+
+    void Start()
+    {
+        if (PhotonNetwork.IsMasterClient)
+        {
+            _myserver = PhotonNetwork.Instantiate("MyServer", transform.position, transform.rotation).GetComponent<MyServer>();          
+            _myserver.winScreen = winScreen;
+  
+        }
+
+    }
+
+
+
+}
