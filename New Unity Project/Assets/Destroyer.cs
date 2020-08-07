8dd0d38a0853695296acd6fb0085284ea09d798f
Belu123mm <32393295+Belu123mm@users.noreply.github.com>
2020-08-07 02:48:20 -0300

    Partikl de WA

---
+++ New Unity Project/Assets/Destroyer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class Destroyer : MonoBehaviourPun
+{
+    public float secs;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if ( PhotonNetwork.IsMasterClient ) {
+            Destroy(this.gameObject, secs);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
