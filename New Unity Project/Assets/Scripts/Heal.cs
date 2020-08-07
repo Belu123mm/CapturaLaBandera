e7a5ce1ea971cb90946fe8705642c383703270b8
Gonzalou <56851977+Gonzalou@users.noreply.github.com>
2020-08-07 04:10:03 -0300

    heal

---
+++ New Unity Project/Assets/Scripts/Heal.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Heal : MonoBehaviour
+{
+    public int heal;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.layer == 9)
+        {
+            PlayerModel model=collision.gameObject.GetComponent<PlayerModel>();
+            Server.Instance.RequestHeal(model,heal);
+        }
+    }
+}
