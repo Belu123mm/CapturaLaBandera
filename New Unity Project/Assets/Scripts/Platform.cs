76e689dcc661f0a15161e49d82d3fe1f528e0237
Guillermo Felix Romero <guillermo.romero@davinci.edu.ar>
2020-12-21 14:00:01 -0300

    plataformita para probar como queda

---
+++ New Unity Project/Assets/Scripts/Platform.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class Platform : MonoBehaviourPun
+{
+    public Transform pos1;
+    public Transform pos2;
+    Transform current;
+    public float speed = 3;
+
+    public float time_stop_platform;
+    float timer_stop;
+    bool timestop = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        current = pos1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (!timestop)
+        {
+            Vector3 dir = current.transform.position - this.transform.position;
+            dir.Normalize();
+            this.transform.position += dir * Time.deltaTime * speed;
+            if (Vector3.Distance(this.transform.position, current.transform.position) <= 0.2f)
+            {
+                current = current == pos1 ? pos2 : pos1;
+                timestop = true;
+                timer_stop = 0;
+            }
+        }
+        else 
+        {
+            if (timer_stop < time_stop_platform) timer_stop = timer_stop + 1 * Time.deltaTime;
+            else { timer_stop = 0; timestop = false; }
+        }
+    }
+}
